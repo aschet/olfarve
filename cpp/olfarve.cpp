4bf8cab620e4d559176691878248f969cfe1fd6b
Thomas Ascher <thomas.ascher@gmx.at>
2024-01-05 12:15:19 +0100

    added missing float spec

--- cpp/olfarve.cpp
+++ cpp/olfarve.cpp
@@ -119,10 +119,10 @@ static float transferColorComponent(float t) {
 // Implemented according to A. J. de Lange, "Color," in Brewing Materials and Processes, Elsevier, 2016, pp. 199-249.
 // Color space is converted to sRGB via https://www.w3.org/Graphics/Color/srgb
 static RGB beerSDToSRGB(const float& a430, const float& l) {
-    float x = 0.0;
-    float y = 0.0;
-    float z = 0.0;
-    float w = 380.0;
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+    float w = 380.0f;
     for (size_t i = 0; i < CIE_DATA.size(); ++i) {
         float t = pow(10.0f, -a430 * l * (0.02465f * exp(-(w - 430.0f) / 17.591f) + 0.97535f * exp(-(w - 430.0f) / 82.122f)));
         float d65 = CIE_DATA[i][3];
