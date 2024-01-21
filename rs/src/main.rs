bc4aa424072c4a4b11dd3e1d4ebe676d4911bc61
Thomas Ascher <thomas.ascher@gmx.at>
2024-01-21 19:58:12 +0100

    added comments to Rust

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,7 +1,7 @@
 mod olfarve;
 
-use crate::olfarve::olfarve::srm_to_srgb;
 use crate::olfarve::olfarve::rgb_to_hex;
+use crate::olfarve::olfarve::srm_to_srgb;
 use crate::olfarve::olfarve::DEFAULT_PATH;
 
 fn main() {
